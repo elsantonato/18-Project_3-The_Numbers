e8549dec0e983e42295f35336db501fd42e3b304
Lisa Santonato <42822556+elsantonato@users.noreply.github.com>
2019-03-16 14:01:59 -0400

    updates

--- ftpmedia_app/app.py
+++ ftpmedia_app/app.py
@@ -36,65 +36,20 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/names")
-def names():
+@app.route("/mydata")
+def mydata():
     """Return a list of sample names."""
 
     # Use Pandas to perform the sql query
-    stmt = db.session.query(myTable).statement
+
+    # RUNS A QUERY ON THE TABLE TO PULL ALL ROWS AND COLUMNS
+    ### CHANGW THIS ##### stmt = db.session.query(myTable).statement
+
+    ## CONVERTS THE OUTPUT OF THE QUERY INTO A PANDAS DATAFRAME
     df = pd.read_sql_query(stmt, db.session.bind)
 
-    # Return a list of the column names (sample names)
-    return jsonify(list(df.columns)[2:])
-
-
-@app.route("/metadata/<sample>")
-def myTable(sample):
-    """Return the MetaData for a given sample."""
-    sel = [
-        myTable.YEAR,
-        myTable.CLIENT,
-        myTable.PROJECT,
-        myTable.ROLE,
-        myTable.TYPE,
-        myTable.INDUSTRY,
-        myTable.EVENT,
-    ]
-
-    results = db.session.query(*sel).all()
-
-    # Create a dictionary entry for each row of metadata information
-    myTable = {}
-    for result in results:
-        myTable["YEAR"] = result[0]
-        myTable["CLIENT"] = result[1]
-        myTable["PROJECT"] = result[2]
-        myTable["ROLE"] = result[3]
-        myTable["TYPE"] = result[4]
-        myTable["INDUSTRY"] = result[5]
-        myTable["EVENT"] = result[6]
-
-    print(myTable)
-    return jsonify(myTable)
-
-
-@app.route("/samples/<sample>")
-def samples(sample):
-    """Return `otu_ids`, `otu_labels`,and `sample_values`."""
-    stmt = db.session.query(Samples).statement
-    df = pd.read_sql_query(stmt, db.session.bind)
-
-    # Filter the data based on the sample number and
-    # only keep rows with values above 1
-    sample_data = df.loc[df[sample] > 1, ["otu_id", "otu_label", sample]]
-    # Format the data to send as json
-    data = {
-        "otu_ids": myTable.otu_id.values.tolist(),
-        "sample_values": myTable[sample].values.tolist(),
-        "otu_labels": myTable.otu_label.tolist(),
-    }
-    return jsonify(data)
-
+    # Return JSON VERSION OF THE OUTPUT FROM THE DATAFRAME IN THE /MYDATA ROUTE
+    return jsonify(df)
 
 if __name__ == "__main__":
     app.run()
